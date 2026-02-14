1c7f66dc485d120141780e108824f547f06d8c9f
jonatha silva <jonatha.sviegas@hotmail.com>
2026-02-14 16:27:53 -0300

    transform carts v2

--- src/transform/transform_carts.py
+++ src/transform/transform_carts.py
@@ -2,41 +2,46 @@ from ..extract.extract import extract_collection
 import pandas as pd
 
 
-data_carts = extract_collection('carts')
+def drop_missing_values(data_carts: pd.DataFrame) -> pd.DataFrame:
+    return data_carts.dropna(subset=['userId', 'products', 'total', 'discountedTotal'])
 
-# print(len(data_carts))
+def drop_inconsistent_values(data_carts: pd.DataFrame) -> pd.DataFrame:
+    data_carts = data_carts[data_carts['total'] >= 0]
+    data_carts = data_carts[data_carts['totalProducts'] >= 0]
+    return data_carts[data_carts['totalQuantity'] >= 0]
 
-# tirar vazios
-# data_cart = data_carts.dropna(subset=['userId', 'products', 'total', 'discountedTotal'])
+def parse_date(date) -> pd.Timestamp:
+    if isinstance(date, int):
+        return pd.to_datetime(date, unit='s')
+    date = str(date)
+    date = date.replace('T', ' ').replace('Z', '')
+    if '-' in date:
+        return pd.to_datetime(date, errors='coerce')
+    if '/' in date:
+        parts = date.split('/')
+        new_date = parts[2]+'-'+parts[1]+'-'+parts[0]
+        return pd.to_datetime(new_date, errors='coerce')
+    return pd.to_datetime(date)
 
-# tratar total price
-# data_carts = data_carts[data_carts['total'] >= 0]
+def transform_transaction_date(data_carts: pd.DataFrame) -> pd.DataFrame:
+    data_carts['transaction_date'] = data_carts['transaction_date'].apply(parse_date)
+    data_carts = data_carts.dropna(subset=['transaction_date'])
+    data_carts['transaction_date'] = data_carts['transaction_date'].dt.date
+    return data_carts
 
-# tratar total product
-# data_carts = data_carts[data_carts['totalProducts'] >= 0]
 
-# tratar totalQuantity
-# data_carts = data_carts[data_carts['totalQuantity'] >= 0]
+def run_etl_carts() -> pd.DataFrame:
+    data_carts = extract_collection('carts')
+    data_carts = drop_missing_values(data_carts)
+    data_carts = drop_inconsistent_values(data_carts)
+    data_carts = transform_transaction_date(data_carts)
+    return data_carts
 
 # TODO: ver se a quantidade de produtos no carrinho é maior q a minima por id de produto
 
-# tratar transaction_date
 
-# def parse_date(date):
-#     if isinstance(date, int):
-#         return pd.to_datetime(date, unit='s')
-#     date = str(date)
-#     date = date.replace('T', ' ').replace('Z', '')
-#     if '-' in date:
-#         return pd.to_datetime(date, errors='coerce')
-#     if '/' in date:
-#         parts = date.split('/')
-#         new_date = parts[2]+'-'+parts[1]+'-'+parts[0]
-#         return pd.to_datetime(new_date, errors='coerce')
-#     return pd.to_datetime(date)
+
     
 
-# data_carts['transaction_date'] = data_carts['transaction_date'].apply(parse_date)
-# data_carts = data_carts.dropna(subset=['transaction_date'])
-# data_carts['transaction_date'] = data_carts['transaction_date'].dt.date
+
 
--- src/transform/transform_products.py
+++ src/transform/transform_products.py
@@ -2,23 +2,23 @@ from ..extract.extract import extract_collection
 import pandas as pd
 
 
-def drop_missing_values(data_products) -> pd.DataFrame:
+def drop_missing_values(data_products: pd.DataFrame) -> pd.DataFrame:
     return data_products.dropna(subset=['title', 'price'])
 
 
-def drop_duplicates_values(data_products) -> pd.DataFrame:
+def drop_duplicates_values(data_products: pd.DataFrame) -> pd.DataFrame:
     unique_fields = ['title', 'sku']
     return data_products.drop_duplicates(subset=unique_fields, keep='first')
 
 
-def drop_spaces(data_products) -> pd.DataFrame:
+def drop_spaces(data_products: pd.DataFrame) -> pd.DataFrame:
     cols = ['title','description','category','brand','sku',
             'warrantyInformation','shippingInformation',
             'availabilityStatus','returnPolicy','thumbnail']
     mask = data_products[cols].apply(lambda x: x.str.strip() != '').all(axis=1)
     return data_products[mask]
 
-def drop_inconsistent_values(data_products) -> pd.DataFrame:
+def drop_inconsistent_values(data_products: pd.DataFrame) -> pd.DataFrame:
     mask = (
         (data_products['price'] >= 0) &
         (data_products['discountPercentage'] >= 0) & (data_products['discountPercentage'] <= 100) &
