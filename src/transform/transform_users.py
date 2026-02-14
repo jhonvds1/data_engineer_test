0a66bcc54a4379c856051471270e0b8fa1cf8a39
jonatha silva <jonatha.sviegas@hotmail.com>
2026-02-14 17:02:47 -0300

    transform users v1

--- src/transform/transform_users.py
+++ src/transform/transform_users.py
@@ -1,121 +1,90 @@
 from ..extract.extract import extract_collection
 import pandas as pd
 
-data_users = extract_collection('users')
+
+def drop_missing_values(data_users: pd.DataFrame) -> pd.DataFrame:
+    return data_users.dropna(subset=['firstName', 'lastName', 'username', 'email', 'password'])
+
+def drop_duplicates_values(data_users: pd.DataFrame) -> pd.DataFrame:
+    unique_fields = ['email', 'username', 'cpf', 'cnpj']
+    return data_users.drop_duplicates(subset=unique_fields, keep='first')
+
+def clean_first_names(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users = data_users[~((data_users['firstName'].str.strip() == '') | (data_users['firstName'].str.len() < 2) | (~data_users['firstName'].str.match(r'^[A-Za-zÀ-ÿ\s]+$')))]
+    data_users['firstName'] = data_users['firstName'].str.strip().str.capitalize()
+    return data_users
+
+def clean_last_names(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users = data_users[~((data_users['lastName'].str.strip() == '') | (data_users['lastName'].str.len() < 2) | (~data_users['lastName'].str.match(r'^[A-Za-zÀ-ÿ\s]+$')))]
+    data_users['lastName'] = data_users['lastName'].str.strip().str.capitalize()
+    return data_users
+
+def clean_maiden_names(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users = data_users[~((~data_users['maidenName'].str.match(r'^[A-Za-zÀ-ÿ\s]+$')) & (data_users['maidenName'] != ''))]
+    data_users['maidenName'] = data_users['maidenName'].str.strip().str.capitalize()
+    return data_users
+
+def drop_inconsistent_values(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users =  data_users[(data_users['age'] >=0) & (data_users['age'] <= 120)]
+    return data_users[(data_users['height'] >= 0) & (data_users['weight'] >= 0)]
 
 
-# ---------- tirar dados faltantes
+def parse_gender(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users['gender'] = data_users['gender'].str.strip().str.lower()
+    data_users['gender'] = data_users['gender'].replace({
+        'm' : 'male',
+        'f' : 'female',
+        'male' : 'male',
+        'female' : 'female'
+    })
+    return data_users
 
-# data_users = data_users.dropna(subset=['firstName', 'lastName', 'username', 'email', 'password'])
-
-# ----------- tratar dados duplicados
-
-# unique_fields = ['email', 'username', 'cpf', 'cnpj']
-# data_users = data_users.drop_duplicates(subset=unique_fields, keep='first')
-
-
-# ---------- tratamento nome
-
-# invalid_names = data_users[(data_users['firstName'].str.strip() == '') | (data_users['firstName'].str.len() < 2) | (~data_users['firstName'].str.match(r'^[A-Za-zÀ-ÿ\s]+$'))]
-# print(f"a: {invalid_names}")
-# data_users['firstName'] = data_users['firstName'].str.strip().str.capitalize
-
-# ---------- tratamento sobrenome
-
-# invalid_names = data_users[(data_users['lastName'].str.strip() == '') | (data_users['lastName'].str.len() < 2) | (~data_users['lastName'].str.match(r'^[A-Za-zÀ-ÿ\s]+$'))]
-# print(f"a: {invalid_names}")
-# data_users['lastName'] = data_users['lastName'].str.strip().str.capitalize
+def clean_phone_numbers(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users['phone'] = data_users['phone'].str.strip()
+    data_users = data_users[data_users['phone'].str.startswith('+')]
+    return data_users
 
 
-# tratamento maidenname
+def clean_username(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users['username'] = data_users['username'].str.strip()
+    data_users['username'] = data_users['username'].str.lower()
+    return data_users
 
-# invalid_names = data_users[(~data_users['maidenName'].str.match(r'^[A-Za-zÀ-ÿ\s]+$')) & (data_users['maidenName'] != '')]
-# data_users['maidenName'] = data_users['maidenName'].str.strip().str.capitalize
+def clean_user_fields(data_users: pd.DataFrame) -> pd.DataFrame:
+    columns_to_strip = [
+        'image', 'bloodGroup', 'eyeColor', 'ip', 'macAddress',
+        'university', 'userAgent', 'role', 'cnpj'
+    ]
+    for col in columns_to_strip:
+        if col in data_users.columns:
+            data_users[col] = data_users[col].astype(str).str.strip()
+    return data_users
 
+def clean_user_email(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users['email'] = data_users['email'].str.strip()
+    data_users['email'] = data_users['email'].str.lower()
+    email_pattern = r'^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'
+    return data_users[data_users['email'].str.match(email_pattern)]
 
-# tratamentp idade
-
-# data_users = data_users[(data_users['age'] >=0) & (data_users['age'] <= 120)]
-
-# tratamento gender
-# data_users['gender'] = data_users['gender'].str.strip().str.lower()
-
-# data_users['gender'] = data_users['gender'].replace({
-#     'm' : 'male',
-#     'f' : 'female',
-#     'male' : 'male',
-#     'female' : 'female'
-# })
-
-
-# tratamento phone
-# data_users['phone'] = data_users['phone'].str.strip()
-# data_users = data_users[data_users['phone'].str.startswith('+')]
-
-#tratamento username
-
-# data_users['username'] = data_users['username'].str.strip()
-# data_users['username'] = data_users['username'].str.lower()
-
-# tratamento password
-
-# data_users['password'] = data_users['password'].str.strip()
-
-#tratamento image
-
-# data_users['image'] = data_users['image'].str.strip()
-
-# tratamento sangue
-
-# data_users['bloodGroup'] = data_users['bloodGroup'].str.strip().str.upper()
-
-# tratamento peso e a ltura
-
-# data_users = data_users[(data_users['height'] >= 0) & (data_users['weight'] >= 0)]
-
-# tratamento cor do olho 
-
-# data_users['eyeColor'] = data_users['eyeColor'].str.strip().str.capitalize()
-
-# tratamento ip
-
-# data_users['ip'] = data_users['ip'].str.strip()
-
-#tratamento macAddress
-
-# data_users['macAddress'] = data_users['macAddress'].str.strip()
-
-# tratamento university
-
-# data_users['university'] = data_users['university'].str.strip().str.capitalize()
-
-# tratamento useragent
-
-# data_users['userAgent'] = data_users['userAgent'].str.strip()
-
-# tratamento role
-
-# data_users['role'] = data_users['role'].str.strip().str.capitalize()
-
-#tratamento cpf
-
-# data_users['cpf'] = data_users['cpf'].str.strip()
-
-#tratamento cnpj
-
-# data_users['cnpj'] = data_users['cnpj'].str.strip()
-
-# tratamento bithdate
-# data_users['birthDate'] = data_users['birthDate'].str.strip()
-# data_users['birthDate'] = pd.to_datetime(data_users['birthDate'], format='%Y-%m-%d')
-
-# tratamento email
-
-# data_users['email'] = data_users['email'].str.strip()
-# data_users['email'] = data_users['email'].str.lower()
-
-# email_pattern = r'^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'
-# data_users = data_users[data_users['email'].str.match(email_pattern)]
+def clean_user_birthdate(data_users: pd.DataFrame) -> pd.DataFrame:
+    data_users['birthDate'] = data_users['birthDate'].str.strip()
+    data_users['birthDate'] = pd.to_datetime(data_users['birthDate'], format='%Y-%m-%d')
+    return data_users
 
 
-# print(data_users.keys())
+
+def run_etl_users() -> pd.DataFrame:
+    data_users = extract_collection('users')
+    data_users = drop_missing_values(data_users)
+    data_users = drop_duplicates_values(data_users)
+    data_users = clean_first_names(data_users)
+    data_users = clean_last_names(data_users)
+    data_users = clean_maiden_names(data_users)
+    data_users = drop_inconsistent_values(data_users)
+    data_users = parse_gender(data_users)
+    data_users = clean_phone_numbers(data_users)
+    data_users = clean_username(data_users)
+    data_users = clean_user_fields(data_users)
+    data_users = clean_user_email(data_users)
+    data_users = clean_user_birthdate(data_users)
+    return data_users
