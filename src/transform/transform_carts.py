401aceb9c5094b9edf324b2bea99e4d52ff214cd
jonatha silva <jonatha.sviegas@hotmail.com>
2026-02-15 13:27:01 -0300

    transform carts v3

--- src/transform/transform_carts.py
+++ src/transform/transform_carts.py
@@ -30,19 +30,45 @@ def transform_transaction_date(data_carts: pd.DataFrame) -> pd.DataFrame:
     return data_carts
 
 def remove_invalid_orders(data_carts: pd.DataFrame) -> pd.DataFrame:
-    ...
-# TODO: ver se a quantidade de produtos no carrinho é maior q a minima por id de produto
+    data_products = extract_collection('products')
 
+    # explode preservando índice do carrinho
+    carts_exploded = data_carts.explode('products').reset_index().rename(columns={'index': 'cart_index'})
+
+    # normaliza produtos
+    products = pd.json_normalize(carts_exploded['products'])
+    products['cart_index'] = carts_exploded['cart_index'].values
+
+    # dados mínimos do produto
+    products_minimum = data_products[['id', 'minimumOrderQuantity']].rename(columns={'id': 'product_id'})
+
+    # produtos do carrinho
+    products_cart = products[['cart_index', 'id', 'quantity']].rename(columns={'id': 'product_id'})
+
+    # join para validação
+    products_check = pd.merge(products_cart, products_minimum, on='product_id', how='inner')
+
+    # carrinhos inválidos
+    invalid_carts = products_check.loc[
+        products_check['quantity'] < products_check['minimumOrderQuantity'],
+        'cart_index'
+    ].unique()
+
+    # filtra carrinhos válidos
+    data_carts = data_carts.drop(index=invalid_carts)
+
+    return data_carts
 
 
 def run_etl_carts() -> pd.DataFrame:
     data_carts = extract_collection('carts')
+    data_carts = remove_invalid_orders(data_carts)
     data_carts = drop_missing_values(data_carts)
     data_carts = drop_inconsistent_values(data_carts)
     data_carts = transform_transaction_date(data_carts)
     return data_carts
 
-
+print(run_etl_carts())
 
 
 
